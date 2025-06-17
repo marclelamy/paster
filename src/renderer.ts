460548c319b88c9f95fc181857fa027c2aeaf127
Marc Lamy <marclelamym@gmail.com>
2025-06-17 18:21:35 +0200

    not in a good place

---
+++ src/renderer.ts
@@ -0,0 +1,12 @@
+import { BrowserWindow } from "electron/main"
+import path from "path"
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+}
